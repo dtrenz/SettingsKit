1d6975219b91005657be454d7c35e1ef2e3a49e0
Dan Trenz <dtrenz@gmail.com>
2016-02-26 11:34:07 -0500

    Initial Commit

---
+++ SettingsKit/SettingsKit.h
@@ -0,0 +1,19 @@
+//
+//  SettingsKit.h
+//  SettingsKit
+//
+//  Created by Dan Trenz on 2/26/16.
+//  Copyright © 2016 Dan Trenz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SettingsKit.
+FOUNDATION_EXPORT double SettingsKitVersionNumber;
+
+//! Project version string for SettingsKit.
+FOUNDATION_EXPORT const unsigned char SettingsKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SettingsKit/PublicHeader.h>
+
+
