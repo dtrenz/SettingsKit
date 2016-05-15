364118586b88d81b191a1f18ae22512df3db1634
bsorrentino <bartolomeo.sorrentino@gmail.com>
2016-05-15 22:13:32 +0200

    fix cli for compatibility with xcodeproj 1.0.0

--- cli/parser.rb
+++ cli/parser.rb
@@ -5,7 +5,8 @@ module SettingsKit
 
     def self.parse(plist_path)
       puts "SettingsKit: Parsing #{plist_path}..."
-      plist = Xcodeproj::PlistHelper.read(plist_path)
+      # plist = Xcodeproj::PlistHelper.read(plist_path)
+      plist = Xcodeproj::Plist.read_from_path(plist_path)
       keys = plist["PreferenceSpecifiers"].map { |i| i["Key"] }.compact.sort
       Renderer.new(keys)
     end
